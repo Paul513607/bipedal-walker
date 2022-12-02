92fd6b732d1e67f70c007f9605bb8067ce40c317
Paul Samson <79131709+Paul513607@users.noreply.github.com>
2022-12-02 16:34:39 +0200

    deep q alg

---
+++ DeepQNetwork.py
@@ -0,0 +1,92 @@
+import random
+from collections import deque
+
+import numpy as np
+from keras import Sequential
+from keras.layers import Dense
+from tensorflow import keras
+
+GAMMA = 0.95
+ALPHA = 0.001
+EPSILON = 1.0
+EPSILON_DECAY = 0.995
+EPSILON_MIN = 0.01
+BATCH_SIZE = 16
+MEMORY_SIZE = 1000
+TARGET_UPDATE = 100
+MIN_REPLAY_SIZE = 100
+
+
+class DeepQNetwork:
+    def __init__(self, state_size, action_size):
+        self.state_size = state_size
+        self.action_size = action_size
+        self.memory = deque(maxlen=MEMORY_SIZE)
+        self.losses = []
+        self.model = self.create_model()
+        self.target_model = self.create_model()
+        self.target_model.set_weights(self.model.get_weights())
+        self.target_update_counter = 0
+
+    def create_model(self):
+        model = Sequential()
+        model.add(Dense(24, input_dim=self.state_size, activation="relu"))
+        model.add(Dense(24, activation="relu"))
+        model.add(Dense(self.action_size, activation="linear"))
+        model.compile(loss="mse", optimizer='rmsprop', metrics=['accuracy'])
+        return model
+
+    def update_replay_memory(self, state, action, reward, new_state, done):
+        self.memory.append((state, action, reward, new_state, done))
+
+    def get_qs(self, state):
+        return self.model.predict(np.array(state).reshape(-1, *state.shape))[0]
+
+    def get_action(self, state, action_space):
+        if np.random.random() < EPSILON:
+            return action_space.sample()
+        else:
+            return self.get_qs(state)
+
+    def train(self, terminal_state, step):
+        if len(self.memory) < MIN_REPLAY_SIZE:
+            return
+
+        minibatch = random.sample(self.memory, BATCH_SIZE)
+        current_states = np.array([transition[0] for transition in minibatch])
+        current_qs_list = self.model.predict(current_states)
+
+        next_states = np.array([transition[3] for transition in minibatch])
+        next_qs_list = self.target_model.predict(next_states)
+
+        X = []
+        y = []
+
+        for index, (current_state, action, reward, new_current_state, done) in enumerate(minibatch):
+            if not done:
+                max_next_q = reward + GAMMA * np.max(next_qs_list[index])
+            else:
+                max_next_q = reward
+
+            current_qs = current_qs_list[index]
+            action_idx = np.argmax(action)
+            current_qs[action_idx] = max_next_q
+
+            X.append(current_state)
+            y.append(current_qs)
+
+        self.model.fit(np.array(X), np.array(y), batch_size=BATCH_SIZE, verbose=0, shuffle=False, callbacks=None)
+
+        if terminal_state:
+            self.target_update_counter += 1
+
+        if self.target_update_counter > TARGET_UPDATE:
+            self.target_model.set_weights(self.model.get_weights())
+            self.target_update_counter = 0
+
+    def update_epsilon(self):
+        global EPSILON
+        EPSILON = max(EPSILON * EPSILON_DECAY, EPSILON_MIN)
+        
+    def get_epsilon(self):
+        return EPSILON
--- main.py
+++ main.py
@@ -1,14 +1,38 @@
+import random
+
 import keras
 import gym
 import time
 import numpy as np
-import matplotlib.pyplot as plt
+import time
+
+from DeepQNetwork import DeepQNetwork
+
+EPIOSDES = 1000
+STEPS = 100
+RENDER = True
 
 if __name__ == '__main__':
     env = gym.make("BipedalWalker-v3")
-    env.reset()
+    state_size = env.observation_space.shape[0]
+    action_size = env.action_space.shape[0]
+    agent = DeepQNetwork(state_size, action_size)
+    for episode in range(EPIOSDES):
+        state = env.reset()
+        score = 0
+        for step in range(STEPS):
+            if RENDER:
+                env.render()
+            action = agent.get_action(state, env.action_space)
+            new_state, reward, done, _ = env.step(action)
+            score += reward
+            agent.update_replay_memory(state, action, reward, new_state, done)
+            agent.train(done, step)
+            state = new_state
+            if step % 10 == 0:
+                print(step)
+            if done:
+                break
+        print(f"Episode: {episode}, Score: {score}, Epsilon: {agent.get_epsilon()}")
+        agent.update_epsilon()
 
-    for _ in range(1000):
-        env.render()
-        env.step(env.action_space.sample())
-    env.close()
